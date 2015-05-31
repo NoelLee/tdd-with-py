9ac7301249b7ad97fd683d0a2cddead1acea13e6
Noel Lee <leekayau@gmail.com>
2015-05-31 20:56:12 +0800

    new add_item URL and view for adding to existing lists. FT PASSES :')

--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,13 @@ def home_page(request):
 def new_list(request):
     new_list = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=new_list)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect("/lists/%d/" % new_list.id)
 
-def view_list(request):
-    return render(request, "list.html", {"items": Item.objects.all()})
+def add_item(request, list_id):
+    requested_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=requested_list)
+    return redirect("/lists/%d/" % requested_list.id)
+
+def view_list(request, list_id):
+    requested_list = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": requested_list})
