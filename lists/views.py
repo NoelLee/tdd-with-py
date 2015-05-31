e1242cbdeadfec1f7d049f9ea01ab7205da690bf
Noel Lee <leekayau@gmail.com>
2015-05-31 16:34:19 +0800

    model for list Items and associated migration

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    print("yay" + request.POST.get("item_text", ""))
     return render(request, "home.html", {
         "new_item_text": request.POST.get("item_text", ""),
     })
